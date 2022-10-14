5921449751f012060400669402a383745277e3b0
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-14 12:22:53 +0200

    completato palindromi

--- palindromo/main.js
+++ palindromo/main.js
@@ -3,6 +3,20 @@ let userWord = prompt("insert a word to check for palindromy")
 if(!isNaN(userWord)){
     alert("Please refresh and insert a word")
 } else {
+    console.log("aa")
     palindromeChecker()
 }
 
+function palindromeChecker(){
+
+    let counter = userWord.length
+
+    for(let i = 0; i < userWord.length; i++){
+
+        if(userWord[i] == userWord[0-counter]){
+            console.log("ok")
+        }
+
+        counter--
+    }
+}
